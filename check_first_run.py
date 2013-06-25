da9993bfc4ca7bb55236057b7588ed41fa40c334
Trung Ngo <ndtrung4419@gmail.com>
2013-06-26 00:25:45 +0700

    Add encoding spec

--- check_first_run.py
+++ check_first_run.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 #
 # This file is part of the gokien project.
 #
--- gokien_welcome.py
+++ gokien_welcome.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf8 -*-
 #
 # This file is part of the gokien project.
 #
