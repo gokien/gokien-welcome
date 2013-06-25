6874134a6c2a14c97a66ffc9c1f3a43c2d8df6a6
Trung Ngo <ndtrung4419@gmail.com>
2013-06-26 00:42:10 +0700

    Remove the last remnants of ibus-bogo-python in the license plates.

--- check_first_run.py
+++ check_first_run.py
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ibus-bogo-python.  If not, see <http://www.gnu.org/licenses/>.
+# along with gokien.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import os
